166f65e48753ee775d489beb4b2963799e6c7c4d
Bonnni <12-slava@mail.ru>
2021-08-27 13:57:38 +0500

    initialization Ef

---
+++ appCRUD/EF/EfDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using appCRUD.EF.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace appCRUD.EF
+{
+    class EfDbContext : DbContext
+    {
+        public EfDbContext(DbContextOptions<EfDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var path = "Server = DESKTOP - QNIVVVU\\SQLEXPRESS; Database = User; Trusted_Connection = True";
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(path);
+            }
+        }
+    }
+}
--- appCRUD/Form1.cs
+++ appCRUD/Form1.cs
@@ -16,5 +16,10 @@ namespace appCRUD
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
